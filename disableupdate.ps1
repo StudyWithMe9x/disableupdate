71c58ef95748a6c2381a8d1afa9cbb7e0860fe95
StudyWithMe9x <remoteathena@gmail.com>
2025-03-27 14:47:48 +0700

    Update disableupdate.ps1

--- disableupdate.ps1
+++ disableupdate.ps1
@@ -1,4 +1,4 @@
 $Path = $env:TEMP; $Installer = "disableupdate.zip"; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://github.com/tsgrgo/windows-update-disabler/archive/refs/heads/main.zip" -OutFile $Path\$Installer;
-Expand-Archive "$Path\disableupdate.zip" -DestinationPath "$Path\disableupdate";
-start $Path\disableupdate;
+Expand-Archive "$Path\disableupdate.zip" -DestinationPath "$Path\disableupdate"
+start $Path\disableupdate
 exit
